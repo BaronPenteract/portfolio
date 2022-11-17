801f6f9d5377a906cb5db1526cd568672600671f
Andrey <andreas42r@yandex.ru>
2022-11-18 00:35:27 +0700

    feature/burgerActiveButton: done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
-console.log("Hi, there! I'm BaronPenteract!")
+/* burger */
+let headerElement = document.querySelector('.header');
+let burgerButtonElement = headerElement.querySelector('.burger');
+let headerNavElement = headerElement.querySelector('.nav');
+let profileElement = document.querySelector('.profile');
+let profileOverlayElement = profileElement.querySelector('.profile__overlay');
+
+/* ------------------------------------------------------------ */
+/* burger */
+burgerButtonElement.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  burgerButtonElement.classList.toggle('burger_active');
+  profileElement.classList.toggle('profile_active');
+})
+
+profileOverlayElement.addEventListener('click', function(e) {
+  if (e.target === e.currentTarget) {
+    burgerButtonElement.classList.toggle('burger_active');
+    profileElement.classList.toggle('profile_active');
+  }
+})
+/* ------------------------------------------------------------ */

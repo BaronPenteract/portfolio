10a8b9dd944ea3a54f89f152e663221c5af9b02d
Andrey <andreas42r@yandex.ru>
2022-11-18 19:19:58 +0700

    fix/headerSublist: hover added

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,10 @@ const headerNavElement = headerElement.querySelector('.nav');
 const profileElement = document.querySelector('.profile');
 const profileOverlayElement = profileElement.querySelector('.profile__overlay');
 
+/* header sublist */
+const navLinkListElement = headerElement.querySelector('.nav__link_list');
+const navSublistElement = headerElement.querySelector('.nav__sublist');
+
 /* ------------------------------------------------------------ */
 /* burger */
 burgerButtonElement.addEventListener('click', function(e) {
@@ -20,4 +24,29 @@ profileOverlayElement.addEventListener('click', function(e) {
     profileElement.classList.toggle('profile_active');
   }
 })
+
+/* nav link list */
+navLinkListElement.addEventListener('mouseover', function(e) {
+  showSublist();
+})
+navLinkListElement.addEventListener('mouseout', function(e) {
+  hideSublist();
+})
+
+navSublistElement.addEventListener('mouseover', function(e) {
+  showSublist();
+})
+navSublistElement.addEventListener('mouseout', function(e) {
+  hideSublist();
+})
+
+function showSublist() {
+  navLinkListElement.classList.add('nav__link_active');
+  navSublistElement.classList.add('nav__sublist_active');
+}
+
+function hideSublist() {
+  navLinkListElement.classList.remove('nav__link_active');
+  navSublistElement.classList.remove('nav__sublist_active');
+}
 /* ------------------------------------------------------------ */

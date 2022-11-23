5e19c21303930a2d508eecbc5afccb2e529c9a90
Andrey <andreas42r@yandex.ru>
2022-11-23 15:12:00 +0700

    feature/posts: done

--- scripts/theme.js
+++ scripts/theme.js
@@ -4,13 +4,13 @@ let theme = 0;
 
 const themes = {
   '--main-text-color': ['#fff', '#000'],
+  '--sub-text-color': ['#dedede', '#343434'],
   '--accent-color': ['#3137c9', '#00c0ff'],
   '--page-bg-color': ['#181818', '#e5e5e5'],
   '--border-color': ['#1c1c1c', '#e1e1e1'],
   '--shadow-color': ['rgba(0, 0, 0, 0.3)', 'rgb(141 141 141 / 30%)'],
 
   '--works-button-color': ['#ED3024', '#ff5247'],
-  '--profile-subtitle-color': ['#dedede', '#343434'],
 
 
   '--handle-bg-color': ['#0d0d0d', '#f3f3f3'],

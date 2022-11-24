7f51662a36ecd04fdf9ead2bb1e23d5466d868a7
Andrey <andreas42r@yandex.ru>
2022-11-24 15:12:25 +0700

    feature/postsFormAddFunctional: добавление постов с текстом и картинкой только. Отрисовка постов из массива инициализирующего)

---
+++ scripts/initialPosts.js
@@ -0,0 +1,60 @@
+const topics = [
+  'создание сайтов',
+  'продвижение видео'
+]
+const types = [
+  'posts__item_type_text',
+  'posts__item_type_image',
+  'posts__item_type_video',
+]
+const initialPosts = [
+  {
+    type: types[1],
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.',
+    time: 'Thu Nov 24 2020 12:53:51 GMT+0700',
+    media: {
+      title: 'Eat, sleep, code, repeat.',
+      url: 'https://cdn2.f-cdn.com/contestentries/215692/11659560/5550154bdddf5_thumb900.jpg',
+    },
+    topic: topics[0],
+  },
+  {
+    type: types[1],
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.',
+    time: 'Thu Nov 24 2020 12:53:51 GMT+0700',
+    media: {
+      title: 'Как я сходил на FrontEnd Conf 2021',
+      url: 'images/posts/post-basic2.jpg',
+    },
+    topic: topics[0],
+  },
+  {
+    type: types[2],
+    text: undefined,
+    media: {
+      title: '2rbina2rista - brutality',
+      url: 'https://www.youtube.com/embed/Gd2OapjhJwg',
+    },
+    time: 'Thu Nov 02 2020 10:03:51 GMT+0700',
+  },
+  {
+    type: types[1],
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.',
+    time: new Date() - 3000000,
+    media: {
+      title: 'Как писать код быстро и безболезненно?',
+      url: '../images/posts/post-basic.jpg',
+    },
+    topic: topics[0],
+  },
+  {
+    type: types[0],
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum volutpat orci turpis urna. Et vestibulum, posuere tortor lacinia sit. Sagittis porttitor orci auctor in at tincidunt arcu egestas. Fusce arcu sodales lacinia eu auctor nunc nam id. Diam sit sed volutpat massa. Egestas ornare vel volutpat.',
+    time: new Date() - 200000,
+    media: {
+      title: undefined,
+      url: undefined
+    },
+    topic: topics[0],
+  },
+]
--- scripts/posts.js
+++ scripts/posts.js
@@ -1,6 +1,99 @@
-const formPost = document.querySelector('.posts__form');
+const postsSection = document.querySelector('.posts');
+const formPost = postsSection.querySelector('.posts__form');
 const inputTextarea = formPost.querySelector('.posts__input');
+const inputText = formPost.querySelector('.posts__input_type_text');
 const btnSubmit = formPost.querySelector('.posts__btn_type_submit');
+const btnAddMedia = formPost.querySelector('.posts__btn_type_add-media');
+
+const postsList = postsSection.querySelector('.posts__list');
+const postTemplate = postsSection.querySelector('#posts-form-add-template').content;
+
+/* ---------------------СОЗДАНИЕ____ПОСТА--------------------------------- */
+function createPost(postObj) {
+  const postElement = postTemplate.cloneNode(true);
+  const postImage = postElement.querySelector('.posts__image');
+  const postIframe = postElement.querySelector('.posts__iframe');
+  const postText = postElement.querySelector('.posts__text');
+  const postTitle = postElement.querySelector('.posts__title');
+  const postTime = postElement.querySelector('.posts__time');
+  const postTopic = postElement.querySelector('.posts__topic');
+  const postLink = postElement.querySelector('.posts__link');
+
+  const timeOfPublish = new Date(postObj.time);
+
+  postElement.querySelector('.posts__item').classList.add(postObj.type)
+
+  const datatime = `${timeOfPublish.getFullYear()}-${timeOfPublish.getMonth() + 1}-${timeOfPublish.getDate()}T${timeOfPublish.getHours()}:${timeOfPublish.getMinutes()}:${timeOfPublish.getSeconds()}`;
+
+  postTime.textContent = timeToText(timeOfPublish);
+  postTime.setAttribute('datetime', datatime);
+
+
+  if(postObj.media.title) {
+    postTitle.textContent = postObj.media.title;
+  } else {
+    postTitle.remove();
+  }
+
+  if(postObj.text) {
+    postText.textContent = postObj.text;
+  } else {
+    postText.remove();
+  }
+
+  if(postObj.type === 'posts__item_type_image') {
+    postImage.src = postObj.media.url;
+    postTopic.textContent = topics[0];
+    postIframe.remove();
+  } else {
+    postImage.remove();
+  }
+
+  if(postObj.type === 'posts__item_type_video') {
+    postIframe.src = postObj.media.url;
+    postTopic.textContent = topics[1];
+    postImage.remove();
+  }
+
+  if(postObj.type === 'posts__item_type_text') {
+    postTopic.remove();
+    postLink.remove();
+    postIframe.remove();
+    postImage.remove();
+  }
+
+  console.log(postObj.type);
+  console.log('Пост создан');
+  return postElement;
+}
+/* -------------------------------------------------------------------------------------- */
+
+function showPost(postElement) {
+  postsList.prepend(postElement);
+}
+
+function timeToText(timeOfPublish) {
+  const nowTime = new Date();
+  const timeText = `${timeOfPublish.getHours()}:${timeOfPublish.getMinutes()}:${timeOfPublish.getSeconds()} ${timeOfPublish.getDate()}.${timeOfPublish.getMonth() + 1}.${timeOfPublish.getFullYear()}`;
+
+  const now = 60000;
+  const fiveMins = 300000;
+  const oneHour = 3600000;
+
+  if (nowTime - timeOfPublish < now ) {
+    return 'Только что'
+  }
+
+  if (nowTime - timeOfPublish < fiveMins ) {
+    return 'Менее 5ти минут назад'
+  }
+
+  if (nowTime - timeOfPublish < oneHour ) {
+    return 'Менее часа назад'
+  }
+
+  return timeText
+}
 
 inputTextarea.addEventListener('keydown', function() {
   setTimeout( function() {
@@ -8,7 +101,34 @@ inputTextarea.addEventListener('keydown', function() {
   }, 1)
 })
 
+let postTitlePromt,
+    postUrlPromt;
+btnAddMedia.addEventListener('click', function() {
+  postTitlePromt = prompt('Title', );
+  postUrlPromt = prompt('URL', );
+})
+
 formPost.addEventListener('submit', function(e) {
   e.preventDefault();
-  console.log('Добавлено)')
+
+  const post = {
+    type: postUrlPromt ? types[1] : types[0],
+    text: inputText.value,
+    time: new Date(),
+    media: {
+      title: postTitlePromt,
+      url: postUrlPromt,
+    },
+  };
+  inputText.value = '';
+  postTitlePromt = undefined;
+  postUrlPromt = undefined;
+
+  showPost(createPost(post));
+  console.log('Добавлено.');
+})
+
+
+initialPosts.forEach(function(post) {
+  showPost(createPost(post))
 })

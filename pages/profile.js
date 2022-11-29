ab774e2111cc42751db31831f62963f1e6d57167
Andrey <andreas42r@yandex.ru>
2022-11-29 13:16:06 +0700

    feature/profilePage: initial

---
+++ pages/profile.js
@@ -0,0 +1,7 @@
+import { activateBurger } from "../blocks/burger/burger.js";
+import { activateTheme } from "../blocks/theme/theme.js";
+import { activateNavPlaceHeader } from "../blocks/nav/nav.js";
+
+activateBurger();
+activateTheme();
+activateNavPlaceHeader();
